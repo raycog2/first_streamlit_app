65ecdff9d341c11568fd2c1af5ba3dcc67a611c4
raycog2 <127772100+raycog2@users.noreply.github.com>
2023-03-14 23:51:33 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,9 +34,9 @@ try:
     else:
       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
       # Dataframe 
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    # write your own comment - what does this do?
-    streamlit.dataframe(fruityvice_normalized)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      # write your own comment - what does this do?
+      streamlit.dataframe(fruityvice_normalized)
     
     #removed 
   #streamlit.write('The user entered ', fruit_choice)
