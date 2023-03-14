e98a62c92e9467605ebe839b2258e621124a54d1
raycog2 <127772100+raycog2@users.noreply.github.com>
2023-03-14 23:59:55 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #create the repeatable block (call a function)
-def get_fruiyvice_data(this_fruit_choice):
+def get_fruityvice_data(this_fruit_choice):
       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
       # Dataframe 
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
@@ -41,7 +41,7 @@ try:
     if not fruit_choice:
       streamlit.error("Please select a fruit to get information.")
     else:
-      back_from_function= get_fruityvice_data(fruit_choice)
+      back_from_function = get_fruityvice_data(fruit_choice)
       streamlit.dataframe(back_from_function)
     
     #removed 
