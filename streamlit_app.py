521bfa60a1b80245416de15df8611808b1c983a7
raycog2 <127772100+raycog2@users.noreply.github.com>
2023-03-14 23:25:42 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,9 +40,10 @@ streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
 
-my_cur.execute("use warehouse compute_wh") 
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
+my_cur.execute("use warehouse compute_wh") 
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchone()
