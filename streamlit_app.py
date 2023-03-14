f69ef888787c8ab7b52e14741b1952891b781804
raycog2 <127772100+raycog2@users.noreply.github.com>
2023-03-14 10:14:59 +0000

    Update streamlit_app.py
    added emohis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, spinach &rocket smoothie')
-streamlit.text('Hard-boiled Free Range egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, spinach &rocket smoothie')
+streamlit.text('🐔 Hard-boiled Free Range egg')
+streamlit.text('🥑🍞 Avocado Toast')
